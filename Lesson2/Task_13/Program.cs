6b4b47fd18ad0e3d98e236ca9743387c29e410bb
PashkevichEvgeny <pash.ev@ya.ru>
2023-01-31 20:18:30 +0800

    Ответы на домашнее задание к второму семинару

---
+++ Lesson2/Task_13/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13:
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+// Математический способ
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 100)
+{
+    Console.WriteLine("третье цифры нет");
+}
+else{
+while (number > 999)
+{
+    number = number / 10;
+}
+int thirdNumber = number % 10;
+Console.WriteLine(thirdNumber);
+}
+
+// Строковый способ
+// Console.Write("Введите число: ");
+// string stringNumber = Console.ReadLine()!;
+// if (stringNumber.Length < 3)
+// {
+//     Console.WriteLine("третье цифры нет");
+//     return;
+// }
+// char thirdSymbol = stringNumber[2];
+// Console.WriteLine(thirdSymbol);
---
+++ Lesson2/Task_15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15:
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int numberDay = int.Parse(Console.ReadLine()!);
+
+// Дотошная проверка
+// while (numberDay > 7 || numberDay < 1)
+// {
+//     Console.Write("Введите номер дня недели от 1 до 7 включительно: ");
+//     numberDay = int.Parse(Console.ReadLine()!);
+// }
+
+if (numberDay > 0 & numberDay < 6 )
+{
+    Console.WriteLine("нет");
+}
+if (numberDay == 6 || numberDay == 7)
+{
+    Console.WriteLine("да");
+}
