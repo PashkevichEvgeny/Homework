c28a7ed0821ed1bfae96ac332f6ca124ec882d13
PashkevichEvgeny <pash.ev@ya.ru>
2023-02-04 12:45:23 +0800

    Ответы на домашнее задание к третьему семинару

---
+++ Lesson3/Task19/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+// Математический способ
+
+Console.Write("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+// Проверка на длину
+// while (n < 10000 || n > 99999)
+// {
+//     Write("Введите число из состоящее из пяти цифр: ");
+//     n = int.Parse(Console.ReadLine()!);
+// }
+
+if (n / 10000     == n % 10 &&
+    n / 1000 % 10 == n % 100 / 10)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
+
+/*
+// Строковый способ
+Console.Write("Введите пятизначное число: ");
+// Принимаем строку и обрезаем впереди стоящие нули
+string digit = Convert.ToString(int.Parse(Console.ReadLine()!));
+
+// Проверка на длину
+while (digit.Length != 5)
+{
+    Console.Write("Введите число из состоящее из пяти цифр: ");
+    digit = Convert.ToString(int.Parse(Console.ReadLine()!));
+}
+
+string isPalindrome = digit[0] == digit[4] &&
+                      digit[1] == digit[3] ? "да" : "нет";
+Console.WriteLine(isPalindrome);
+*/
---
+++ Lesson3/Task21/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+// Двумерный массив с названиями точек
+char[,] pointName = {{'A', 'x'}, {'A', 'y'}, {'A', 'z'}, 
+                       {'B', 'x'}, {'B', 'y'}, {'B', 'z'}};
+int amountOfPoints = pointName.GetLength(0);
+int[] coordArray = new int[amountOfPoints];
+
+for (int i = 0; i < amountOfPoints; i++)
+{
+    Console.Write($"Введите координату точки {pointName[i, 0]} по оси {pointName[i, 1]}: ");
+    coordArray[i] = int.Parse(Console.ReadLine()!);
+}
+double distance = Math.Sqrt(Math.Pow((coordArray[0] - coordArray[3]), 2) +
+                            Math.Pow((coordArray[1] - coordArray[4]), 2) +
+                            Math.Pow((coordArray[2] - coordArray[5]), 2));
+Console.WriteLine($"{distance:f2}");
+
+/*
+// Сокращенный вариант. Запись трех координат в один массив
+Console.Clear();
+Console.Write("Введите координаты первой точки через запятую: ");
+int[] A = Array.ConvertAll(Console.ReadLine()!.Split(','), int.Parse);
+Console.Write("Введите координаты второй точки через запятую: ");
+int[] B = Array.ConvertAll(Console.ReadLine()!.Split(','), int.Parse);
+
+double dis = Math.Sqrt(Math.Pow((B[0] - A[0]), 2) +
+                       Math.Pow((B[1] - A[1]), 2) +
+                       Math.Pow((B[2] - A[2]), 2));
+Console.WriteLine($"{Math.Round(dis, 2)}");
+*/
