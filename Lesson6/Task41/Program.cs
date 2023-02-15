865e2abfc895899e6d7111ac447c3b39237d1186
PashkevichEvgeny <pash.ev@ya.ru>
2023-02-15 17:44:46 +0800

    Домашнее задание Шестой семинар

---
+++ Lesson6/Task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41:
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 4
+
+int[] InputArray()
+{
+    Console.Write("Введите элементы массива через пробел и/или запятую: ");
+    string[] arrayString = Console.ReadLine()!.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    int len = arrayString.Length;
+    int[] arrayInt = new int[len];
+    for (int i = 0; i < len; i++)
+        arrayInt[i] = int.Parse(arrayString[i].Trim());
+    return arrayInt;
+}
+
+int ItemMoreZero(int[] array)
+{
+    int count = 0;
+    foreach (int item in array) if (item > 0) count++;
+    return count;
+}
+
+int[] userArray = InputArray();
+Console.WriteLine($"[{String.Join(", ", userArray)}]");
+int number = ItemMoreZero(userArray);
+Console.WriteLine($"Чисел больше нуля: {number}");
---
+++ Lesson6/Task43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double[] Coord()
+{
+    string[] name = { "b1", "k1", "b2", "k2" };
+    double[] arrayCoord = new double[4];
+    for (int i = 0; i < arrayCoord.Length; i++)
+    {
+        Console.Write($"Введите данные {name[i]}: ");
+        arrayCoord[i] = double.Parse(Console.ReadLine()!);
+    }
+    return arrayCoord;
+}
+
+
+bool Cross(double b1, double k1, double b2, double k2, out double x, out double y)
+{
+    if (k1 == k2 || b1 == b2)
+    {
+        x = double.NaN;
+        y = double.NaN;
+        return false;
+    }
+    
+    y = (k1 * b2 - k2 * b1) / (k1 - k2);
+    x = (y - b1) / k1;
+    return true;
+}
+
+double[] a = Coord();
+bool answer = Cross(a[0], a[1], a[2], a[3], out double x, out double y);
+if (answer)
+{
+    System.Console.WriteLine($"{x} {y}");
+}
+else
+{
+    Console.WriteLine("Прямые совпадают или параллельны");
+}
